6324a935b1f922dae261e29f42cafae034c2d46f
Rafaellopes011 <rafaellopes250106@gmail.com>
2024-11-26 15:42:29 -0300

    Delete Teste directory

--- Teste/Teste/src/Registravel.java
+++
@@ -1,6 +0,0 @@
-import java.io.IOException;
-
-interface Registravel {
-    void salvarEmArquivo(String caminho) throws IOException;
-    void carregarDeArquivo(String caminho) throws IOException;
-}
--- Teste/Teste/src/Transacionavel.java
+++
@@ -1,4 +0,0 @@
-interface Transacionavel {
-    void realizarTransferencia();
-    void cancelarTransferencia();
-}
